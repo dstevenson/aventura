7b105c845b06e8ea361accaa0d735248d0e94993
Sean Gajewski <srgajewski@gmail.com>
2013-04-28 21:13:49 -0400

    fuck doug

---
+++ aventura/projects.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Template Name: Projects
+*/
+get_header(); ?>
+
+        <div id="content">
+            
+            <div id="leftContent">
+ adfafa
+            </div> 
+            
+            <div id="rightContent">
+                
+                <?php the_post(); ?>
+                
+		<h5><?php the_title(); ?></h5>
+                
+            </div> 
+        
+        </div>
+        
+        <div style="clear:both;"></div>
