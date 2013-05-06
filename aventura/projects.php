25e98796e687e684c69d561c2d3836631a41f4bf
Doug Stevenson <dstevenson@centraldesktop.com>
2013-05-05 19:25:07 -0700

    Update link

--- aventura/projects.php
+++ aventura/projects.php
@@ -73,7 +73,7 @@ get_header(); ?>
                 <table cellpadding="0" cellspacing="0" width="100%" style="margin-left: 55px;">
                     <tr>
                         <td><a href="<?php echo home_url(); ?>/aventura-projects/general-construction/"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/projects_construction.jpg" border="0"></a></td>
-                        <td><a href="<?php echo home_url(); ?>/aventura-projects/maintenance_service/"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/projects_maintenance.jpg" border="0"></a></td>
+                        <td><a href="<?php echo home_url(); ?>/aventura-projects/maintenance-service/"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/projects_maintenance.jpg" border="0"></a></td>
                         <td><a href="<?php echo home_url(); ?>/aventura-projects/environmental-services/"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/projects_environmental.jpg" border="0"></a></td>
                     </tr>    
                 </table>
